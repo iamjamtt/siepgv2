6e06e400ca7bf7c4721aed4f4bf06624f77cf648
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2023-12-18 00:06:07 -0500

    Update index.blade.php

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,3 +1,48 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="d-flex flex-column flex-column-fluid">
+    <div id="kt_app_toolbar" class="app-toolbar py-3">
+        <div id="kt_app_toolbar_container" class="app-container container-fluid d-flex flex-stack">
+            <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+                <h2 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
+                    Home
+                </h2>
+                <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                    <li class="breadcrumb-item text-muted">
+                        Home
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="kt_app_content" class="app-content flex-column-fluid">
+        <div id="kt_app_content_container" class="app-container container-fluid">
+            <div class="row mb-5 mb-xl-10">
+                <div class="col-md-12 mb-md-5 mb-xl-10">
+                    <div class="card card-dashed">
+                        <div class="card-body">
+                            <div class="card-px text-center py-md-10 my-5">
+                                <h2 class="fs-2hx fw-bold mb-5">
+                                    Bienvenidos al
+                                    <br>
+                                    Sistema Integrado de la Escuela de Posgrado
+                                </h2>
+
+                                <h3 class="fs-2x fw-bold mb-10">
+                                    SIEPG
+                                </h3>
+
+                                <div class="text-center px-4 mb-10">
+                                    <img class="mw-100 mh-300px" alt="logo"
+                                        src="{{ asset('assets/media/logo-pg.png') }}">
+                                </div>
+
+                                <p class="text-gray-700 fs-2 fw-semibold">
+                                    Universidad Nacional de Ucayali
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
