196bad63a0fb0db272c50b34006a07583132f726
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2023-12-17 22:25:47 -0500

    Refactor app layout and add toastr notifications

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -48,58 +48,24 @@
 
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
         <livewire:components.navbar-app>
-        <div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
-            <livewire:components.sidebar-app>
-            <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
-                <div class="d-flex flex-column flex-column-fluid">
-                    <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
-                        <div id="kt_app_toolbar_container" class="app-container container-fluid d-flex flex-stack">
-                            <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
-                                <h2
-                                    class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
-                                    Home
-                                </h2>
-                                <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                                    <li class="breadcrumb-item text-muted">
-                                        <a href="index.html" class="text-muted text-hover-primary">
-                                            Dashboard
-                                        </a>
-                                    </li>
-                                    <li class="breadcrumb-item">
-                                        <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                                    </li>
-                                    <li class="breadcrumb-item text-muted">
-                                        Home
-                                    </li>
-                                </ul>
+            <div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+                <livewire:components.sidebar-app>
+                    <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+                        {{ $slot }}
+                        <div id="kt_app_footer" class="app-footer">
+                            <div
+                                class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
+                                <div class="text-gray-900 order-2 order-md-1">
+                                    <span class="text-muted fw-semibold me-1">{{ date('Y') }}&copy;</span>
+                                    <a href="https://keenthemes.com" target="_blank"
+                                        class="text-gray-800 text-hover-primary">
+                                        Siepg - Escuela de Posgrado
+                                    </a>
+                                </div>
                             </div>
-                            {{-- <div class="d-flex align-items-center gap-2 gap-lg-3">
-                                <a href="#" class="btn btn-sm fw-bold btn-secondary" data-bs-toggle="modal"
-                                    data-bs-target="#kt_modal_create_app">Rollover</a>
-                                <a href="#" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#kt_modal_new_target">Add Target</a>
-                            </div> --}}
                         </div>
                     </div>
-                    <div id="kt_app_content" class="app-content flex-column-fluid">
-                        <div id="kt_app_content_container" class="app-container container-fluid">
-                            Hola mundo
-                        </div>
-                    </div>
-                </div>
-                <div id="kt_app_footer" class="app-footer">
-                    <div
-                        class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
-                        <div class="text-gray-900 order-2 order-md-1">
-                            <span class="text-muted fw-semibold me-1">{{ date('Y') }}&copy;</span>
-                            <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">
-                                Siepg - Escuela de Posgrado
-                            </a>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </div>
     </div>
 
     <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
@@ -113,6 +79,44 @@
 
     <script src="{{ asset('assets/js/widgets.bundle.js') }}"></script>
     <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+
+
+    <script>
+        document.addEventListener('livewire:navigated', () => {
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": true,
+                "positionClass": "toastr-top-center",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+            window.addEventListener('toast', event => {
+                if (event.detail.tipo == 'success') {
+                    toastr.success(event.detail.mensaje, event.detail.titulo);
+                } else if (event.detail.tipo == 'error') {
+                    toastr.error(event.detail.mensaje, event.detail.titulo);
+                } else if (event.detail.tipo == 'warning') {
+                    toastr.warning(event.detail.mensaje, event.detail.titulo);
+                } else if (event.detail.tipo == 'info') {
+                    toastr.info(event.detail.mensaje, event.detail.titulo);
+                }
+            })
+
+            window.addEventListener('modal', event => {
+                $(event.detail.modal).modal(event.detail.action);
+            })
+        })
+    </script>
 </body>
 
 </html>
